5831029b3091d7d39e01191b967802f0c3975f70
Jerry applegarth <applegarth1630@gmail.com>
2022-08-26 19:34:45 -0700

    2.3 splash with animation

--- app/src/main/java/com/applecompose/thebook/presentation/screens/splashscreen/SplashScreen.kt
+++ app/src/main/java/com/applecompose/thebook/presentation/screens/splashscreen/SplashScreen.kt
@@ -1,5 +1,7 @@
 package com.applecompose.thebook.presentation.screens.splashscreen
 
+import android.view.animation.OvershootInterpolator
+import androidx.compose.animation.Animatable
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
@@ -7,18 +9,39 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
+import androidx.compose.ui.draw.scale
+import kotlinx.coroutines.delay
 
 @Composable
 fun SplashScreen(navController: NavController) {
+	val scale = remember {
+		Animatable(0f)
+	}
+	LaunchedEffect(key1 = true) {
+		scale.animateTo(targetValue = 0.9f,
+		animationSpec = tween(800,
+		easing = {
+			OvershootInterpolator(8f)
+				.getInterpolation(it)
+		}))
+		delay(2000L)
+
+	}
+
+
 	Surface(modifier = Modifier
 		.padding(16.dp)
-		.size(330.dp),
+		.size(330.dp)
+		.scale(scale.value),
 		shape = CircleShape,
 		color = Color.White,
 		border = BorderStroke(
