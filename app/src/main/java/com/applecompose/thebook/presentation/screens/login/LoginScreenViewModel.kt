4d302576bd1886089926f340320630008adfe3d4
Jerry applegarth <applegarth1630@gmail.com>
2022-08-28 18:00:50 -0700

    3.0 starting auth with FB

---
+++ app/src/main/java/com/applecompose/thebook/presentation/screens/login/LoginScreenViewModel.kt
@@ -0,0 +1,48 @@
+package com.applecompose.thebook.presentation.screens.login
+
+import android.content.ContentValues.TAG
+import android.util.Log
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.launch
+import java.lang.Exception
+
+class LoginScreenViewModel: ViewModel() {
+
+	//val loadingState = MutableStateFlow(LoadingState.IDLE)
+	private val auth: FirebaseAuth = Firebase.auth
+
+	private val _loading = MutableLiveData(false)
+	val loading: LiveData<Boolean> = _loading
+
+	fun signInWithEmailAndPassword(email: String, password: String)
+	= viewModelScope.launch{
+		try {
+			auth.signInWithEmailAndPassword(email, password)
+				.addOnCompleteListener { task ->
+					if (task.isSuccessful) {
+						TODO("take them home")
+					}else {
+						Log.d("FB", "signInWithEmailAndPassword: ${task.result.toString()}")
+					}
+				}
+		}catch (ex: Exception) {
+			Log.d("FB", "signInWithEmailAndPassword: ${ex.message}")
+		}
+	}
+
+	fun createUserEmailAndPassword() {
+
+
+	}
+
+
+
+
+}
